4941190260db1968b925a7cc43aa1ba2c7860834
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2024-03-19 14:17:45 +0500

    add tailwind directives

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Routing from 'routing'
 
-import 'tailwindcss/tailwind.css'
+import 'styles/main.css'
 
 const container = document.getElementById('root')
 if (container) {
