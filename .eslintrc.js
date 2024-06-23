1670418a156e2f372f65fe456845ff16769ae717
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2024-06-23 15:17:14 +0500

    update eslint and tsconfig configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,75 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	plugins: ['@typescript-eslint/eslint-plugin', 'react', 'prettier'],
-	parser: '@typescript-eslint/parser',
-	extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	ignorePatterns: ['build', '.eslintrc.js'],
-	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
-	rules: {
-		'prettier/prettier': ['warn'],
-		camelcase: [0, { properties: 'never' }],
-		'@typescript-eslint/camelcase': [0, { properties: 'never' }],
-		'react/prop-types': [0],
-		'@typescript-eslint/no-use-before-define': [0],
-		'@typescript-eslint/ban-ts-comment': [0, { properties: 'never' }],
-		'@typescript-eslint/ban-types': 0,
-		'@typescript-eslint/explicit-module-boundary-types': 0,
-		'@typescript-eslint/member-delimiter-style': [
-			'error',
-			{
-				multiline: {
-					delimiter: 'none'
-				}
-			}
-		],
-		'prefer-const': [0],
-		'@typescript-eslint/prefer-const': [0, { destructuring: 'any' }],
-		'@typescript-eslint/explicit-function-return-type': [0],
-		'@typescript-eslint/no-explicit-any': [0],
-		'react/display-name': ['warn'],
-		'react/no-unescaped-entities': ['warn'],
-		'react/no-deprecated': ['warn'],
-		'@typescript-eslint/no-empty-interface': ['warn'],
-		'react/jsx-key': ['warn'],
-		'@typescript-eslint/no-extra-semi': ['warn'],
-		'@typescript-eslint/no-empty-function': [0],
-		'@typescript-eslint/no-unused-vars': [0],
-		'react/jsx-uses-react': 0,
-		'react/react-in-jsx-scope': 0,
-		"semi": ["error", "never"],
-		'@typescript-eslint/no-extra-semi': ['warn'],
-	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	}
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  globals: {
+    "import.meta": "readonly"
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['build', '.eslintrc.js'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'prettier/prettier': ['warn'],
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none'
+        }
+      }
+    ],
+    'prefer-const': 'off',
+    '@typescript-eslint/prefer-const': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/display-name': 'warn',
+    'react/no-unescaped-entities': 'warn',
+    'react/no-deprecated': 'warn',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    'react/jsx-key': 'warn',
+    '@typescript-eslint/no-extra-semi': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'semi': ['error', 'never']
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    },
+    react: {
+      version: 'detect'
+    }
+  }
 }
